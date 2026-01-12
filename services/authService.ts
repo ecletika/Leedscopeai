b3d21807e1ae8ff391aada6a3e6fc5c7fec83746
ecletika <83987553+ecletika@users.noreply.github.com>
2026-01-12 01:12:09 +0000

    Create authService.ts

---
+++ services/authService.ts
@@ -0,0 +1,15 @@
+// services/authService.ts
+import { supabase } from "./supabaseClient.ts";
+
+export async function loginUser(email: string, password: string) {
+  const { data, error } = await supabase.auth.signInWithPassword({
+    email,
+    password,
+  });
+
+  if (error) {
+    return { success: false, message: error.message };
+  }
+
+  return { success: true, user: data.user };
+}
