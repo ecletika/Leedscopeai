56e0ffff671cb539cd7c3c9b9abfe7d36ace65a8
ecletika <83987553+ecletika@users.noreply.github.com>
2026-01-12 01:18:46 +0000

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,3 +1,10 @@
+const { createClient } = require('@supabase/supabase-js');
+
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_ANON_KEY;
+
+const supabase = createClient(supabaseUrl, supabaseKey);
+
 const express = require('express');
 const nodemailer = require('nodemailer');
 const cors = require('cors');
